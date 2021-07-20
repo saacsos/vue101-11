905acc79a3396aae56ea829ff6e9c53afc61f59d
Saac Sornchai <sornchai.la@ku.th>
2021-07-20 14:13:44 +0700

    update pokemon and call json file

--- src/components/PokemonCreateForm.vue
+++ src/components/PokemonCreateForm.vue
@@ -5,66 +5,64 @@
     <div>
       <div>
         <label for="name">Name (En)</label>
-        <input type="text" v-model="form.name.english">
+        <input type="text" v-model="form.name.english" />
       </div>
 
       <div>
         <label for="name_jp">Name (Jp)</label>
-        <input type="text" v-model="form.name.japanese">
+        <input type="text" v-model="form.name.japanese" />
       </div>
 
       <div>
         <label for="type">Type</label>
-        <input type="text" v-model="form.type">
+        <input type="text" v-model="form.type" />
       </div>
 
       <div>
         <button @click="addPokemon">Add</button>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
+import PokemonStore from "@/store/Pokemon"
+
 export default {
   data() {
     return {
       form: {
         name: {
-          english: '',
-          japanese: ''
+          english: "",
+          japanese: "",
         },
-        type: ''
-      }
+        type: "",
+      },
     }
   },
   methods: {
     clearForm() {
       this.form = {
         name: {
-          english: '',
-          japanese: ''
+          english: "",
+          japanese: "",
         },
-        type: ''
+        type: "",
       }
     },
     addPokemon() {
       let payload = {
         name: this.form.name,
-        type: this.form.type.split(",").map(
-          (item) => item.trim()
-        )
+        type: this.form.type.split(",").map((item) => item.trim()),
       }
       // todo: เรียก action ใน PokemonStore เพื่อเพิ่มข้อมูล
       console.log(payload)
+      PokemonStore.dispatch("addPokemon", payload)
 
       this.clearForm()
-    }
-  }
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/store/Pokemon.js
+++ src/store/Pokemon.js
@@ -1,5 +1,9 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import Axios from 'axios'
+
+let api_endpoint =
+  "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
 
 Vue.use(Vuex)
 
@@ -13,40 +17,39 @@ export default new Vuex.Store({
     pokemons: (state) => state.data,
   },
 
-  // mutations เหมือน private setter ใน oop, 
+  // mutations เหมือน private setter ใน oop,
   // เอาไว้เปลี่ยนแปลงค่าใน state
   mutations: {
-    fetch (state, { res }) {
+    fetch(state, { res }) {
       state.data = res.data
-    }
+    },
+    add(state, { payload }) {
+      state.data.push(payload)
+    },
+    edit(state, { payload }) {
+      state.data[payload.index].name = payload.name
+      state.data[payload.index].type = payload.type
+    },
   },
 
-  // actions เหมือน public methods ใน oop, 
+  // actions เหมือน public methods ใน oop,
   // ให้ภายนอกเรียกใช้ หรือ ดึงข้อมูลจากภายนอก
   actions: {
     // commit เป็น keyword ไว้เรียก mutation
-    fetchPokemon ({ commit }) {
-      // สมมติไปเรียกข้อมูลจาก api
-      let res = {
-        data: [
-          {
-            name: {
-              english: "Bulbasaur",
-              japanese: "Fushikidane",
-            },
-            type: ["Grass", "Poison"],
-          },
-          {
-            name: {
-              english: "Bulbasaur 2",
-              japanese: "Fushikidane 2",
-            },
-            type: ["Grass", "Poison"],
-          },
-        ],
-      }
-      commit('fetch', { res })
-    }
+    async fetchPokemon({ commit }) {
+      let res = await Axios.get(api_endpoint)
+      commit("fetch", { res })
+    },
+
+    addPokemon({ commit }, payload) {
+      // todo: call api to add data
+      commit("add", { payload })
+    },
+
+    editPokemon({ commit }, payload) {
+      // todo: call api to edit data
+      commit("edit", { payload })
+    },
   },
   modules: {},
 })
