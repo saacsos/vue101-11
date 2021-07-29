0c88b0483040c571906b18b382fa536cfe824c3a
Saac Sornchai <sornchai.la@ku.th>
2021-07-29 09:58:52 +0700

    fixed: edit pokemon

--- src/store/PokedexApi.js
+++ src/store/PokedexApi.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import Vuex from "vuex"
 import Axios from "axios"
 
-let api_endpoint = process.env.POKEMON_ENDPOINT || 'http://localhost:1337'
+let api_endpoint = process.env.POKEMON_ENDPOINT || "http://localhost:1337"
 
 Vue.use(Vuex)
 
@@ -25,8 +25,8 @@ export default new Vuex.Store({
     add(state, payload) {
       state.data.push(payload)
     },
-    edit (state, payload) {
-      state.data[payload.index] = payload.data
+    edit(state, index, data) {
+      state.data[index] = data
     },
   },
 
@@ -43,7 +43,7 @@ export default new Vuex.Store({
       let url = api_endpoint + "/monsters"
       let body = {
         name: payload.name,
-        name_jp: payload.name_jp
+        name_jp: payload.name_jp,
       }
 
       // todo: หาว่า type เป็น id อะไร
@@ -58,18 +58,20 @@ export default new Vuex.Store({
       commit("add", data)
     },
 
-    async editPokemon ({ commit }, payload) {
+    async editPokemon({ commit }, payload) {
+      let qs = payload.pokemon_types.map((it) => "name_in=" + it).join("&")
+      let res_types = await Axios.get(api_endpoint + "/types?" + qs)
+
       let url = api_endpoint + "/monsters/" + payload.id
       let body = {
         name: payload.name,
-        name_jp: payload.name_jp
+        name_jp: payload.name_jp,
+        pokemon_types: res_types.data.map((it) => it.id),
       }
       let res = await Axios.put(url, body)
       if (res.status === 200) {
-        let data = {}
-        data['data'] = res.data
-        data['index'] = payload.index
-        commit("edit", data)
+        console.log("commit('edit')", payload.index, res.data)
+        commit("edit", payload.index, res.data)
       } else {
         console.err(res)
       }
